2fa7403ae1e6b00ba5f777e52b75005dd9a2609d
Fadel Matar <fadel.matar@gmail.com>
2026-02-25 10:03:28 -0500

    removed tiktok and reader related files

--- components/config/r2.js
+++ components/config/r2.js
@@ -1,3 +1,25 @@
+/**
+ * r2.js - Cloudflare R2 Storage Configuration
+ *
+ * Purpose:
+ *   Sets up and exports an S3-compatible client for Cloudflare R2 object storage.
+ *   Used to upload, retrieve, and delete files (such as article images) in the backend.
+ *
+ * Usage:
+ *   - Imports S3Client from AWS SDK and configures it for Cloudflare R2 using environment variables.
+ *   - Exports the configured client as 's3'.
+ *
+ * Where Used:
+ *   - Article model and controller (for image upload/delete)
+ *   - Any backend code needing access to R2 storage
+ *
+ * Referenced in:
+ *   - components/Article/model.js
+ *   - components/Article/controller.js
+ *
+ * Environment Variables Required:
+ *   - R2_ACCOUNT_ID, R2_ACCESS_KEY_ID, R2_SECRET_ACCESS_KEY
+ */
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
