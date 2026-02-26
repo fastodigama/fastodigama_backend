88c8534d32eae8d9cb0066c26dc769d2e3b61b46
Fadel Matar <fadel.matar@gmail.com>
2026-02-25 22:49:27 -0500

    Remove obsolete code from routes.js; file now empty and safe to delete

--- components/User/routes.js
+++ components/User/routes.js
@@ -1,11 +1,3 @@
 // This file is now empty. All user routes have been split into publicUserRoutes.js and adminUserRoutes.js.
 // You may safely remove this file if not needed.
 
-export default {};
-router.post("/api/login", userController.apiLogin);
-
-//frontend logout
-router.post("/api/logout", userController.apiLogout);
-
-export default router;
-
